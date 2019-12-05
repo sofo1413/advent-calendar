14b0b8f0a404986e859b2d927df80ee2127b121f
Ally Faller <alfa8405@colorado.edu>
2019-12-05 10:47:48 -0700

    added countdown

--- src/Calendar.js
+++ src/Calendar.js
@@ -13,33 +13,38 @@ class Calendar extends React.Component {
 
 
     render() {
+        var daysTill = 25-this.state.today;
+
         return(
-            <div className="container">
-                <Day className="days" today={this.state.today} day={1}/>
-                <Day className="days" today={this.state.today} day={2}/>
-                <Day className="days" today={this.state.today} day={3}/>
-                <Day className="days" today={this.state.today} day={4}/>
-                <Day className="days" today={this.state.today} day={5}/>
-                <Day className="days" today={this.state.today} day={6}/>
-                <Day className="days" today={this.state.today} day={7}/>
-                <Day className="days" today={this.state.today} day={8}/>
-                <Day className="days" today={this.state.today} day={9}/>
-                <Day className="days" today={this.state.today} day={10}/>
-                <Day className="days" today={this.state.today} day={11}/>
-                <Day className="days" today={this.state.today} day={12}/>
-                <Day className="days" today={this.state.today} day={13}/>
-                <Day className="days" today={this.state.today} day={14}/>
-                <Day className="days" today={this.state.today} day={15}/>
-                <Day className="days" today={this.state.today} day={16}/>
-                <Day className="days" today={this.state.today} day={17}/>
-                <Day className="days" today={this.state.today} day={18}/>
-                <Day className="days" today={this.state.today} day={19}/>
-                <Day className="days" today={this.state.today} day={20}/>
-                <Day className="days" today={this.state.today} day={21}/>
-                <Day className="days" today={this.state.today} day={22}/>
-                <Day className="days" today={this.state.today} day={23}/>
-                <Day className="days" today={this.state.today} day={24}/>
-                <Day className="days" today={this.state.today} day={25}/>
+            <div>
+                <h2> Christmas is in {daysTill} days!</h2>
+                <div className="container">
+                    <Day className="days" today={this.state.today} day={1}/>
+                    <Day className="days" today={this.state.today} day={2}/>
+                    <Day className="days" today={this.state.today} day={3}/>
+                    <Day className="days" today={this.state.today} day={4}/>
+                    <Day className="days" today={this.state.today} day={5}/>
+                    <Day className="days" today={this.state.today} day={6}/>
+                    <Day className="days" today={this.state.today} day={7}/>
+                    <Day className="days" today={this.state.today} day={8}/>
+                    <Day className="days" today={this.state.today} day={9}/>
+                    <Day className="days" today={this.state.today} day={10}/>
+                    <Day className="days" today={this.state.today} day={11}/>
+                    <Day className="days" today={this.state.today} day={12}/>
+                    <Day className="days" today={this.state.today} day={13}/>
+                    <Day className="days" today={this.state.today} day={14}/>
+                    <Day className="days" today={this.state.today} day={15}/>
+                    <Day className="days" today={this.state.today} day={16}/>
+                    <Day className="days" today={this.state.today} day={17}/>
+                    <Day className="days" today={this.state.today} day={18}/>
+                    <Day className="days" today={this.state.today} day={19}/>
+                    <Day className="days" today={this.state.today} day={20}/>
+                    <Day className="days" today={this.state.today} day={21}/>
+                    <Day className="days" today={this.state.today} day={22}/>
+                    <Day className="days" today={this.state.today} day={23}/>
+                    <Day className="days" today={this.state.today} day={24}/>
+                    <Day className="days" today={this.state.today} day={25}/>
+                </div>
             </div>
         )
     }
